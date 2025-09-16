fd55e948c86ee19a4237da147512d6f8e8eeb171
himabindu-github <99520700+himabindu-github@users.noreply.github.com>
2025-09-16 18:55:53 +0530

    Add files via upload

---
+++ Stage 1/scripts/run_vfdb_abricate.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+INPUT_DIR="/home/maa/himabindu/results/SPAdes_output"
+OUTPUT_FILE="/home/maa/himabindu/reports/abricate_resf_results.tsv"  # You can change this
+DB="vfdb"
+# Empty the output file first
+> "$OUTPUT_FILE"
+
+# loop through each subdirectory in the input folder
+for subdir in "$INPUT_DIR"/*; do
+  if [[ -d "$subdir" ]]; then
+    CONTIG_FILE="$subdir/contigs.fasta"     #get the contigs.fasta file and assign it to a variable CONTIG_FILE
+    SAMPLE_NAME=$(basename "$subdir")       # extract the sample name
+
+    # Check if contigs.fasta exists and is not from SRR27013337
+    if [[ -f "$CONTIG_FILE" && "$SAMPLE_NAME" != SRR27013337* ]]; then
+      echo "Running abricate on: $SAMPLE_NAME"
+
+      abricate --db "$DB" "$CONTIG_FILE" | sed "s/^/$SAMPLE_NAME\t/" >> "$OUTPUT_FILE"
+    else
+      echo "Skipping: $SAMPLE_NAME"
+    fi
+  fi
+done
