cd57bebaafedc3ae6e5091aae8c37ad24358223c
himabindu-github <99520700+himabindu-github@users.noreply.github.com>
2025-09-16 23:00:15 +0530

    Update blast_all_samples.sh

--- Stage 1/scripts/blast_all_samples.sh
+++ Stage 1/scripts/blast_all_samples.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Directories
+# Directories for input and output
 CONTIGS_DIR="spades_contigs"
-EXTRACTED_DIR="extracted_contigs"
-BLAST_RESULTS_DIR="blast_results"
+EXTRACTED_DIR="extracted_contigs"     # extract top contigs from each contigs.fasta into this folder
+BLAST_RESULTS_DIR="blast_results"     # output
 DB_NAME="listeria_db"
 
 mkdir -p "$EXTRACTED_DIR"
@@ -12,14 +12,19 @@ mkdir -p "$BLAST_RESULTS_DIR"
 # Number of contigs to extract
 NUM_CONTIGS=5
 
+# Loop through input directory, extract sample name
 for sample_fasta in "$CONTIGS_DIR"/*.fasta; do
     sample_name=$(basename "$sample_fasta" .fasta)
     echo "Processing sample: $sample_name"
 
-    # Extract top N longest contigs
+    # Extract top 5 longest contigs
     extracted_fasta="$EXTRACTED_DIR/${sample_name}_top${NUM_CONTIGS}_contigs.fasta"
+
+    # Sorts the sequences in the input FASTA file by length -l, -r reverse the order (longest first)
+   # then selects the top N sequences, and saves them to a new FASTA file.
     seqkit sort -l -r "$sample_fasta" | seqkit head -n $NUM_CONTIGS > "$extracted_fasta"
-    
+
+    # if the file doen't exist, skip
     if [[ ! -s "$extracted_fasta" ]]; then
         echo "No sequences extracted for $sample_name, skipping BLAST."
         continue
